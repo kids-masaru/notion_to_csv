05d4ed0c9374b1410e44bf912778690509624da9
kids-masaru <kids.masaru@gmail.com>
2025-06-20 04:30:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -61,11 +61,11 @@ def extract_page_id_from_url(url):
 # 新規データベース作成関数 (日付プロパティを含まない)
 def create_database(notion, page_id, name):
     properties = {
-        "名前": {"title": [{"text": {"content": str(row["名前"])}}]},
-        "作業順": {"multi_select": [{"name": tag.strip()} for tag in str(row["作業順"]).split(',') if tag.strip()]},
-        "対応": {"select": {"name": str(row["対応"]).strip()}},
-        "担当": {"select": {"name": str(row["担当"]).strip()}},
-    }
+        "名前": {"title": [{"text": {"content": str(row["名前"])}}]},
+        "作業順": {"multi_select": [{"name": tag.strip()} for tag in str(row["作業順"]).split(',') if tag.strip()]},
+        "対応": {"select": {"name": str(row["対応"]).strip()}},
+        "担当": {"select": {"name": str(row["担当"]).strip()}},
+    }
     
     try:
         new_db = notion.databases.create(
